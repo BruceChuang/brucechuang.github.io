868a8f30efa781868b4fc9cef7fc27bbcea2805e
BruceChuang <ZDW1345284190@qq.com>
2016-05-24 13:36:36 +0800

    33

--- js/blog.js
+++ js/blog.js
@@ -1,12 +1,4 @@
-/*背景颜色设置*/
-var $head_setting_img=document.getElementById("head_setting");
+/*点击设置图标事件*/
+var $head_setting_img=document.getElementById("head_setting_img");
 $head_setting_img.onclick=function(){
-	var $head_setting=document.getElementById("head_setting");
-	$head_setting.style.transition="height 0.3s ease";
-	if($head_setting.style.height=="60px"){
-		$head_setting.style.height="140px";
-	}else{
-		$head_setting.style.height="60px";
-	}
-}
-/*背景颜色设置结束*/
+}/*设置结束*/

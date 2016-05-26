e3b6d6615c37a5b3c3a10a7b6177e51ad29dacdd
BruceChuang <ZDW1345284190@qq.com>
2016-05-26 13:17:43 +0800

    11

--- js/index.js
+++ js/index.js
@@ -1,7 +1,7 @@
 /*导航条内容*/
 var $CaseBox_contant_ul_li1=document.getElementById("CaseBox_contant_ul_li1");
 $CaseBox_contant_ul_li1.onclick=function(){
-	var $education_box_resume=document.getElementById("education_box_resume");
-	alert($education_box_resume.scrollTop);
+	var $box=document.getElementById("box");
+	alert($box.offsetTop);
 }
 /*导航条内容---结束*/

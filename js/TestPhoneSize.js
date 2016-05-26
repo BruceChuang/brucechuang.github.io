510ef71703b8850bac3d4da6c83b5c6f51e4d4bc
BruceChuang <ZDW1345284190@qq.com>
2016-05-26 23:27:31 +0800

    11

--- js/TestPhoneSize.js
+++ js/TestPhoneSize.js
@@ -1,4 +1,1 @@
-/*分辨率小于480px时**********************************/	
-document.getElementById("box")
-("<h1>宽"+window.innerWidth+"<br />高"+window.innerHeight+"</div>");
-/*分辨率小于480px时---结束**********************************/	
+document.getElementById("box_test").innerHTML="<h1>宽"+window.innerWidth+"<br />高"+window.innerHeight+"</div>";
